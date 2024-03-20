22d2e0b698775f574b0d1aa192f12570c5ee7664
jcmdo <163624286+jcmdo@users.noreply.github.com>
2024-03-20 14:26:58 +0800

    Create

---
+++ app/components/AddLeave.tsx
@@ -0,0 +1,13 @@
+import { FaPlus } from "react-icons/fa";
+const AddLeave = ()=>{
+  return (
+  <div>
+    
+    <button className='btn btn-primary w-full mb-5 text-base 100 text-xl'> Add Leave
+    <FaPlus className='ml-2' size={18}/> </button>
+
+    </div>
+  );
+};
+
+export default AddLeave;
